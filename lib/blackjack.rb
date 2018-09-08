383438887ba8f9ced4880877c8261d7a27592b46
Stephen Courtier <steves0089@gmail.com>
2018-09-08 18:52:00 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,7 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
   if input == "s"
-    return false 
+    return card_total
   elsif input == "h"
     next_card = deal_card
     card_total = card_total + next_card
